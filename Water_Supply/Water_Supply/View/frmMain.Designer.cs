348f2cedc9f6bbdf02def8e3b9aa72c94fcf9b84
LasithMadhawa <lasithmgunathilake@gmail.com>
2018-07-09 22:23:48 +0530

    Test commit
    Bill Payment button image changed

--- Water_Supply/Water_Supply/View/frmMain.Designer.cs
+++ Water_Supply/Water_Supply/View/frmMain.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.panel1 = new System.Windows.Forms.Panel();
             this.Slider = new System.Windows.Forms.Label();
             this.btnAdmin = new System.Windows.Forms.Button();
@@ -56,19 +57,17 @@
             this.panel1.Controls.Add(this.btnBillPayment);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(228, 750);
+            this.panel1.Size = new System.Drawing.Size(171, 609);
             this.panel1.TabIndex = 0;
             // 
             // Slider
             // 
             this.Slider.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.Slider.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.Slider.Location = new System.Drawing.Point(-4, 158);
-            this.Slider.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Slider.Location = new System.Drawing.Point(-3, 128);
             this.Slider.Name = "Slider";
-            this.Slider.Size = new System.Drawing.Size(23, 87);
+            this.Slider.Size = new System.Drawing.Size(17, 71);
             this.Slider.TabIndex = 5;
             // 
             // btnAdmin
@@ -76,10 +75,10 @@
             this.btnAdmin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAdmin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.btnAdmin.Location = new System.Drawing.Point(0, 562);
+            this.btnAdmin.Location = new System.Drawing.Point(0, 457);
             this.btnAdmin.Margin = new System.Windows.Forms.Padding(0);
             this.btnAdmin.Name = "btnAdmin";
-            this.btnAdmin.Size = new System.Drawing.Size(228, 103);
+            this.btnAdmin.Size = new System.Drawing.Size(171, 84);
             this.btnAdmin.TabIndex = 4;
             this.btnAdmin.Text = "Admin";
             this.btnAdmin.UseVisualStyleBackColor = true;
@@ -90,10 +89,9 @@
             this.btnMember.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnMember.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMember.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.btnMember.Location = new System.Drawing.Point(0, 460);
-            this.btnMember.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMember.Location = new System.Drawing.Point(0, 374);
             this.btnMember.Name = "btnMember";
-            this.btnMember.Size = new System.Drawing.Size(228, 103);
+            this.btnMember.Size = new System.Drawing.Size(171, 84);
             this.btnMember.TabIndex = 3;
             this.btnMember.Text = "Members";
             this.btnMember.UseVisualStyleBackColor = true;
@@ -104,10 +102,9 @@
             this.btnCurrentReading.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnCurrentReading.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCurrentReading.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.btnCurrentReading.Location = new System.Drawing.Point(0, 358);
-            this.btnCurrentReading.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCurrentReading.Location = new System.Drawing.Point(0, 291);
             this.btnCurrentReading.Name = "btnCurrentReading";
-            this.btnCurrentReading.Size = new System.Drawing.Size(228, 103);
+            this.btnCurrentReading.Size = new System.Drawing.Size(171, 84);
             this.btnCurrentReading.TabIndex = 2;
             this.btnCurrentReading.Text = "Current Reading";
             this.btnCurrentReading.UseVisualStyleBackColor = true;
@@ -118,10 +115,9 @@
             this.btnBillGenerate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnBillGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBillGenerate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.btnBillGenerate.Location = new System.Drawing.Point(0, 256);
-            this.btnBillGenerate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBillGenerate.Location = new System.Drawing.Point(0, 208);
             this.btnBillGenerate.Name = "btnBillGenerate";
-            this.btnBillGenerate.Size = new System.Drawing.Size(228, 103);
+            this.btnBillGenerate.Size = new System.Drawing.Size(171, 84);
             this.btnBillGenerate.TabIndex = 1;
             this.btnBillGenerate.Text = "Bill Generate";
             this.btnBillGenerate.UseVisualStyleBackColor = true;
@@ -132,12 +128,12 @@
             this.btnBillPayment.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnBillPayment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBillPayment.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.btnBillPayment.Location = new System.Drawing.Point(0, 154);
-            this.btnBillPayment.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBillPayment.Image = ((System.Drawing.Image)(resources.GetObject("btnBillPayment.Image")));
+            this.btnBillPayment.Location = new System.Drawing.Point(0, 125);
             this.btnBillPayment.Name = "btnBillPayment";
-            this.btnBillPayment.Size = new System.Drawing.Size(228, 103);
+            this.btnBillPayment.Size = new System.Drawing.Size(171, 84);
             this.btnBillPayment.TabIndex = 0;
-            this.btnBillPayment.Text = "Bill Payment";
+            this.btnBillPayment.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             this.btnBillPayment.UseVisualStyleBackColor = true;
             this.btnBillPayment.Click += new System.EventHandler(this.btnBillPayment_Click);
             // 
@@ -145,10 +141,9 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pictureBox1.Location = new System.Drawing.Point(228, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(171, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1143, 62);
+            this.pictureBox1.Size = new System.Drawing.Size(857, 50);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -157,9 +152,9 @@
             this.userControlAdmin1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.userControlAdmin1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.userControlAdmin1.Location = new System.Drawing.Point(0, 0);
-            this.userControlAdmin1.Margin = new System.Windows.Forms.Padding(5);
+            this.userControlAdmin1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.userControlAdmin1.Name = "userControlAdmin1";
-            this.userControlAdmin1.Size = new System.Drawing.Size(1371, 750);
+            this.userControlAdmin1.Size = new System.Drawing.Size(1028, 609);
             this.userControlAdmin1.TabIndex = 5;
             // 
             // userControl1BillGenerate1
@@ -167,9 +162,9 @@
             this.userControl1BillGenerate1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.userControl1BillGenerate1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.userControl1BillGenerate1.Location = new System.Drawing.Point(0, 0);
-            this.userControl1BillGenerate1.Margin = new System.Windows.Forms.Padding(5);
+            this.userControl1BillGenerate1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.userControl1BillGenerate1.Name = "userControl1BillGenerate1";
-            this.userControl1BillGenerate1.Size = new System.Drawing.Size(1371, 750);
+            this.userControl1BillGenerate1.Size = new System.Drawing.Size(1028, 609);
             this.userControl1BillGenerate1.TabIndex = 4;
             // 
             // userControlBillPayment1
@@ -178,9 +173,9 @@
             this.userControlBillPayment1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.userControlBillPayment1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.userControlBillPayment1.Location = new System.Drawing.Point(0, 0);
-            this.userControlBillPayment1.Margin = new System.Windows.Forms.Padding(5);
+            this.userControlBillPayment1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.userControlBillPayment1.Name = "userControlBillPayment1";
-            this.userControlBillPayment1.Size = new System.Drawing.Size(1371, 750);
+            this.userControlBillPayment1.Size = new System.Drawing.Size(1028, 609);
             this.userControlBillPayment1.TabIndex = 2;
             // 
             // userControlCurrentReadingCur1
@@ -189,10 +184,10 @@
             this.userControlCurrentReadingCur1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.userControlCurrentReadingCur1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.userControlCurrentReadingCur1.Location = new System.Drawing.Point(0, 0);
-            this.userControlCurrentReadingCur1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
-            this.userControlCurrentReadingCur1.MaximumSize = new System.Drawing.Size(2000, 2000);
+            this.userControlCurrentReadingCur1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.userControlCurrentReadingCur1.MaximumSize = new System.Drawing.Size(1500, 1625);
             this.userControlCurrentReadingCur1.Name = "userControlCurrentReadingCur1";
-            this.userControlCurrentReadingCur1.Size = new System.Drawing.Size(1371, 750);
+            this.userControlCurrentReadingCur1.Size = new System.Drawing.Size(1028, 609);
             this.userControlCurrentReadingCur1.TabIndex = 7;
             // 
             // userControlMember1
@@ -201,16 +196,16 @@
             this.userControlMember1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.userControlMember1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.userControlMember1.Location = new System.Drawing.Point(0, 0);
-            this.userControlMember1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.userControlMember1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.userControlMember1.Name = "userControlMember1";
-            this.userControlMember1.Size = new System.Drawing.Size(1371, 750);
+            this.userControlMember1.Size = new System.Drawing.Size(1028, 609);
             this.userControlMember1.TabIndex = 8;
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1371, 750);
+            this.ClientSize = new System.Drawing.Size(1028, 609);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.userControlBillPayment1);
@@ -218,9 +213,8 @@
             this.Controls.Add(this.userControlMember1);
             this.Controls.Add(this.userControlAdmin1);
             this.Controls.Add(this.userControl1BillGenerate1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(2661, 2449);
+            this.MaximumSize = new System.Drawing.Size(2000, 1997);
             this.Name = "frmMain";
             this.Text = "Water Board";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
